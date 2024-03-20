1503d9362526d1ebc3ef44974f0b9754848c2898
Scott Kirkland <srkirkland@gmail.com>
2024-03-20 16:42:01 -0700

    much better

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,25 @@
+import { auth } from '@/auth';
 import React from 'react';
 
 const Page = () => {
   return (
-    <div className="container">
-      <div className="jumbotron">
-        <h1 className="display-4">Welcome to Policy Wonk!</h1>
-        <p className="lead">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    <div className='container'>
+      <div className='jumbotron'>
+        <h1 className='display-4'>
+          Welcome to Policy Wonk <UserNameDisplay />!
+        </h1>
+        <p className='lead'>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
       </div>
     </div>
   );
+};
+
+const UserNameDisplay = async () => {
+  const session = await auth();
+
+  return <>{session?.user?.name}!</>;
 };
 
 export default Page;
