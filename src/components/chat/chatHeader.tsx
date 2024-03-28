9547107b7608b799fdf786661c473f815692f133
Cal Doval <cydoval@mac.com>
2024-03-28 16:12:52 -0600

    lede

--- src/components/chat/chatHeader.tsx
+++ src/components/chat/chatHeader.tsx
@@ -12,9 +12,7 @@ const ChatHeader: React.FC = ({}) => {
         src={Logo}
         alt='Aggie Gold Robot cartoon'
       />
-      <h2 className='main-title'>Policy Wonk</h2>
-      <h3 className='sub-title'>Your UC Policy Expert</h3>
-      <p className='intro-text'>
+      <p className='lede'>
         Meet Policywonk, your personal guide to navigating all the ins and outs
         of UC policies...
       </p>
