6c48c58c4178569813773317b9544f3ecce5e9be
Cal Doval <cydoval@mac.com>
2024-03-28 18:02:24 -0600

    focus and visible form work

--- src/components/layout/sidebarLinks.tsx
+++ src/components/layout/sidebarLinks.tsx
@@ -1,17 +1,33 @@
 'use client';
 
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 
-import Link from 'next/link';
+// import Link from 'next/link';
 
 const SidebarLinks: React.FC = () => {
   return (
     <div>
-      <div>
+      {/* <div>
         <Link href='#'>Internal Link</Link>
-      </div>
-      <div>
-        <a href='#'>External Link</a>
+      </div> */}
+      <div className='external-links'>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://computing.caes.ucdavis.edu/'
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </a>
+        <a
+          target='_blank'
+          rel='noopener noreferrer'
+          href='https://github.com/ucdavis/PolicyWonk'
+          className='ps-2'
+        >
+          <FontAwesomeIcon icon={faInfoCircle} />
+        </a>
       </div>
     </div>
   );
