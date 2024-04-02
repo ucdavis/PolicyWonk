d2d9e3e01a72bc5061bae0f7b4b17f19e95dcea3
Cal Doval <cydoval@mac.com>
2024-04-02 12:54:10 -0600

    login page work

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -5,21 +5,36 @@ import { signIn } from '@/auth';
 const Login: React.FC = () => {
   return (
     <>
-      <h1>Login</h1>
-      <form
-        action={async () => {
-          'use server';
-          // TODO: get the callbackUrl from the query params
+      <div className='col-10 login-card'>
+        <img
+          className='img-fluid mb-2'
+          width={55}
+          src='/media/policy-wonk.svg'
+          alt='cartoon robot'
+        />
+        <h1>Welcome to Policy Wonk</h1>
+        <p className='lede'>
+          This tool will help you answer your UCD policy questions, just login,
+          type your question and viola Policy Wonk will do it's best to answer
+          it!
+        </p>
+        <form
+          action={async () => {
+            'use server';
+            // TODO: get the callbackUrl from the query params
 
-          await signIn('azure-ad', {
-            redirectTo: '/',
-          });
-        }}
-      >
-        <button type='submit' className='btn btn-primary btn-lg'>
-          Login
-        </button>
-      </form>
+            await signIn('azure-ad', {
+              redirectTo: '/',
+            });
+          }}
+        >
+          <div className='d-grid'>
+            <button type='submit' className='btn btn-primary btn-lg btn-block'>
+              UC Login
+            </button>
+          </div>
+        </form>
+      </div>
     </>
   );
 };
