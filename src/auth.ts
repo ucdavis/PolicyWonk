06bca0e4a4a155b4ce3a6358acaa20bc6275dae6
Scott Kirkland <srkirkland@gmail.com>
2024-04-04 14:14:48 -0700

    finally figured out how to set userId

--- src/auth.ts
+++ src/auth.ts
@@ -15,4 +15,28 @@ export const {
       authorization: `https://login.microsoftonline.com/${process.env.AZURE_TENANT_ID}/oauth2/v2.0/authorize?response_type=code&domain_hint=ucdavis.edu`,
     }),
   ],
+  callbacks: {
+    async jwt(params) {
+      // params.account has token info -- use to get picture or additional info from provider
+      // params.profile has user info -- oid is the unique user id, email, name also useful.
+      if (params.trigger === 'signIn') {
+        // profile is only available on sign in
+        const token = {
+          ...params.token,
+          name: params.profile?.name,
+          email: params.profile?.email,
+          userId: params.profile?.oid,
+        };
+
+        return token;
+      } else {
+        return params.token;
+      }
+    },
+    async session(params) {
+      params.session.user.id = (params.token.userId as string) || '';
+
+      return params.session;
+    },
+  },
 });
