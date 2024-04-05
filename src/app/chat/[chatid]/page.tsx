d70bbbdd8a90c449e4187025b9732ab3086e5fb6
Scott Kirkland <srkirkland@gmail.com>
2024-04-05 09:56:39 -0700

    remove todo, don't want to todo it

--- src/app/chat/[chatid]/page.tsx
+++ src/app/chat/[chatid]/page.tsx
@@ -17,7 +17,6 @@ const ChatPage = async ({ params: { chatid } }: HomePageProps) => {
   const session = await auth();
 
   if (session?.user) {
-    // TODO: Look into https://react.dev/reference/react/experimental_taintObjectReference
     // filter out sensitive data before passing to client.
     session.user = {
       name: session.user.name,
