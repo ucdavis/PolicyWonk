f122d2a397c6cb609f82d0f1f2ae0fc4773fb07f
River Holstege <laholstege@ucdavis.edu>
2024-04-24 11:13:19 -0700

    fix FA initial load

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,6 @@
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css'; // Import the CSS
+config.autoAddCss = false; // Tell Font Awesome to skip adding the CSS automatically since it's being imported above
 import type { Metadata } from 'next';
 
 import './styles/main.scss';
