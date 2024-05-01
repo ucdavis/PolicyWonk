7feb59c12b630b8d61966ec1ffa4dd3b9672427f
Scott Kirkland <srkirkland@gmail.com>
2024-05-01 10:15:59 -0700

    add in the options, a little animation

--- src/components/chat/focusOptions.tsx
+++ src/components/chat/focusOptions.tsx
@@ -11,7 +11,11 @@ interface FocusOptionsProps {
   onSelection: (selection?: Focus) => void;
 }
 
-const FocusOptions: React.FC<FocusOptionsProps> = ({ open, onSelection }) => {
+const FocusOptions: React.FC<FocusOptionsProps> = ({
+  open,
+  onSelection,
+  options,
+}) => {
   const onClose = () => {
     onSelection();
   };
@@ -19,12 +23,26 @@ const FocusOptions: React.FC<FocusOptionsProps> = ({ open, onSelection }) => {
     <div>
       <Modal isOpen={open} toggle={onClose} centered>
         <ModalHeader toggle={onClose}>Focus Options</ModalHeader>
-        <ModalBody>Hello I'm a modal</ModalBody>
-        <ModalFooter>
-          <Button color='secondary' onClick={onClose}>
-            Close
-          </Button>
-        </ModalFooter>
+        <ModalBody>
+          <div className='container py-2'>
+            <div className='row g-4'>
+              {options.map((option) => (
+                <div className='col-md-4 col-lg-3' key={option.name}>
+                  <div
+                    className='card h-100 focus-card'
+                    style={{ cursor: 'pointer' }}
+                    onClick={() => onSelection(option)}
+                  >
+                    <div className='card-body'>
+                      <h5 className='card-title'>{option.name}</h5>
+                      <p className='card-text'>{option.description}</p>
+                    </div>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        </ModalBody>
       </Modal>
     </div>
   );
