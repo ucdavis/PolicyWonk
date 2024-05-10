5cb865988b15c8ee0043095b9abf8c6771585861
Cal Doval <cydoval@mac.com>
2024-05-10 14:50:41 -0600

    commented removal

--- src/components/chat/focusOptions.tsx
+++ src/components/chat/focusOptions.tsx
@@ -79,15 +79,6 @@ const FocusOptions: React.FC<FocusOptionsProps> = ({
                   <h4>{option.name}</h4>
                   <p>{option.description}</p>
                 </Button>
-                {/* <div
-                  className={`card h-100 focus-card ${option.name === focus.name ? 'selected-focus' : ''}`}
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => onFocusSelection(option)}
-                >
-                  <div className='card-body'>
-                    
-                  </div>
-                </div> */}
               </div>
             ))}
           </div>
