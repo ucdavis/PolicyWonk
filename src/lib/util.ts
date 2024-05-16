80508b0f7378fc79550ec3d78374bd46f8a96d8a
River Holstege <laholstege@ucdavis.edu>
2024-05-16 16:37:16 -0700

    dont show name, catch if !aiState?.messages

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,13 +1,18 @@
 import { ChatHistory } from '@/models/chat';
 
 export const getFullQuestionAndAnswer = (aiState: ChatHistory) => {
+  if (!aiState?.messages) {
+    // TODO: throw error
+    return '';
+  }
+
   const copyableText = aiState.messages
     .filter(
       (message) => message.role === 'user' || message.role === 'assistant'
     )
     .map((message) => {
       return message.role === 'user'
-        ? `${aiState.user ?? 'You'}:\n${message.content}`
+        ? `${message.content}`
         : `Policy Wonk:\n${message.content}`;
     })
     .join('\n\n');
