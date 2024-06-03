11758fa852e5a28a5a57f087503897579cdf877a
River Holstege <laholstege@ucdavis.edu>
2024-06-03 14:39:13 -0700

    less annoying checks

--- src/lib/gtagProvider.tsx
+++ src/lib/gtagProvider.tsx
@@ -9,12 +9,6 @@ const GtagProvider: React.FC = ({}) => {
   if (!gtmId || gtmId === 'GTM-FAKEID') {
     console.warn('GTM_ID not set');
   }
-  if (isProd && gtmDevAuth) {
-    console.warn('GTM_DEV_CONTAINER is set in production');
-  }
-  if (!isProd && !gtmDevAuth) {
-    console.warn('GTM_DEV_CONTAINER not set');
-  }
 
   return (
     <GoogleTagManager gtmId={gtmId} auth={isProd ? undefined : gtmDevAuth} />
