db3a68d7c214144e96dbb21c6e5964230f0a3c1c
River Holstege <laholstege@ucdavis.edu>
2024-07-23 09:42:34 -0700

    sentry

--- src/app/global-error.tsx
+++ src/app/global-error.tsx
@@ -1,6 +1,9 @@
 'use client';
 import React from 'react';
 
+import * as Sentry from '@sentry/nextjs';
+import Error from 'next/error';
+
 import ChatHeader from '@/components/chat/chatHeader';
 import MobileSidebar from '@/components/layout/mobileSidebar';
 import Sidebar from '@/components/layout/sidebar';
@@ -11,7 +14,10 @@ import WonkyErrorBoundary from '@/lib/error/wonkyErrorBoundary';
  * Global error page. This will only show up in production.
  */
 
-const GlobalError: React.FC = () => {
+function GlobalError({ error }: { error: Error }) {
+  React.useEffect(() => {
+    Sentry.captureException(error);
+  }, [error]);
   return (
     <html lang='en'>
       <body>
@@ -40,6 +46,6 @@ const GlobalError: React.FC = () => {
       </body>
     </html>
   );
-};
+}
 
 export default GlobalError;
