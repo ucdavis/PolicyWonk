ceafdff7f1fecba56e909c961958c608a1b0d8ca
Scott Kirkland <srkirkland@gmail.com>
2025-03-13 16:30:09 -0700

    make sure roles are in the db

---
+++ backend/alembic/versions/daa196374d8b_add_in_roles.py
@@ -0,0 +1,28 @@
+"""add in roles
+
+Revision ID: daa196374d8b
+Revises: 7a55612dd058
+Create Date: 2025-03-13 23:25:19.402423
+
+"""
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = 'daa196374d8b'
+down_revision: Union[str, None] = '7a55612dd058'
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    # create ADMIN and USER roles
+    op.execute("INSERT INTO roles (name) VALUES ('ADMIN'), ('USER')")
+
+
+def downgrade() -> None:
+    # remove ADMIN and USER roles
+    op.execute("DELETE FROM roles WHERE name IN ('ADMIN', 'USER')")
