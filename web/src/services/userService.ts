a4904917356f0113bfefebaf3d8ad77997a32dc5
Scott Kirkland <srkirkland@gmail.com>
2025-03-13 13:54:55 -0700

    initial prisma checkin

---
+++ web/src/services/userService.ts
@@ -0,0 +1,7 @@
+export async function ensureUserExists(userId: string) {
+  try {
+    // make sure the user exists in the db with the role user
+  } catch (error) {
+    // return AggieServerError();
+  }
+}
