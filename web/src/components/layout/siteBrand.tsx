a652c0d346783a992cc762ab2a9fc350476d6320
Scott Kirkland <srkirkland@gmail.com>
2025-05-19 14:00:27 -0700

    use url to determine brand

--- web/src/components/layout/siteBrand.tsx
+++ web/src/components/layout/siteBrand.tsx
@@ -1,27 +1,31 @@
+'use client';
 import React from 'react';
 
 import Image from 'next/image';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
-import ucdavislogodark from '/public/media/ucdavis_dark.svg';
+import { campusMap, DEFAULT_GROUP } from '@/lib/constants';
 
 const SiteBrand = () => {
+  const pathname = usePathname();
+  const pathGroup = pathname?.split('/')[1];
+  const group = pathGroup && campusMap[pathGroup] ? pathGroup : DEFAULT_GROUP;
+  const { logoPath, link } = campusMap[group];
+  const chatHref = `/${group}/chat/new`;
+
   return (
     <div className='wonk-header d-flex container justify-content-between align-items-center'>
       <div>
-        <Link
-          className='site-brand'
-          href={'/chat/new'}
-          title='Start a new chat'
-        >
+        <Link className='site-brand' href={chatHref} title='Start a new chat'>
           <h1 className='mb-0'>PolicyWonk</h1>
         </Link>
 
         <p className='discreet m-0'>Beta v0.87</p>
       </div>
       <div className='text-end'>
-        <Link href={'https://www.ucdavis.edu/'}>
-          <Image src={ucdavislogodark} alt='Logo' width={100} />
+        <Link href={link} target='_blank' rel='noopener noreferrer'>
+          <Image src={logoPath} alt='Logo' width={100} height={20} />
         </Link>
       </div>
     </div>
