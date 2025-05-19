4820f51088c531fa901b1b3a0ae8f789142e4d92
Scott Kirkland <srkirkland@gmail.com>
2025-05-19 14:20:06 -0700

    just go to groups page for now

--- web/src/components/layout/footer.tsx
+++ web/src/components/layout/footer.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { faCaretDown } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import Image from 'next/image';
+import Link from 'next/link';
 
 import Ucoplogo from '/public/media/uc_wordmark_blue_official.svg';
 
@@ -14,9 +15,11 @@ const Footer: React.FC = () => {
     <footer>
       <div className='footer-campus-chooser mb-3'>
         <h3 className='mb-0'>Campus</h3>
-        <button className='btn btn-link btn-icon'>
-          UC Davis <FontAwesomeIcon icon={faCaretDown} />
-        </button>
+        <Link href='/groups' passHref>
+          <button className='btn btn-link btn-icon'>
+            UC Davis <FontAwesomeIcon icon={faCaretDown} />
+          </button>
+        </Link>
       </div>
       <FooterLinks />
       <a target='_blank' rel='noopener noreferrer' href='https://ucop.edu'>
