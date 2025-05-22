277bb582c4583f9114ba69181e4a417d6aac4ea2
Cal <cydoval@mac.com>
2025-05-22 14:03:57 -0700

    big push

--- web/src/components/layout/staticNav.tsx
+++ web/src/components/layout/staticNav.tsx
@@ -1,11 +1,20 @@
-const StaticNav = () => {
+import React from 'react';
+
+import DiscreetLinksList, { DiscreetLinks } from '../ui/discreetLinksList';
+
+interface SidebarNavProps {
+  links: DiscreetLinks[];
+  children?: React.ReactNode;
+}
+
+const SidebarNavLinks: React.FC<SidebarNavProps> = ({ children, links }) => {
   return (
-    <div className='wonk-static-wrapper'>
-      <div className='static-page-scroll'>
-        <div className='static-page-wrapper'>test</div>
-      </div>
+    <div className='static-nav-list mb-auto'>
+      <h2>Navigation</h2>
+      <DiscreetLinksList links={links} />
+      {children}
     </div>
   );
 };
 
-export default StaticNav;
+export default SidebarNavLinks;
