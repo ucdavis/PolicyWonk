b3070da25c195c497ecce53a185261fbed06dd9a
Scott Kirkland <srkirkland@gmail.com>
2025-06-03 15:54:23 -0700

    make sure we have valid chat

--- web/src/app/(home)/chat/[chatid]/page.tsx
+++ web/src/app/(home)/chat/[chatid]/page.tsx
@@ -1,5 +1,6 @@
 import { redirect } from 'next/navigation';
 
+import { isWonkSuccess } from '@/lib/error/error';
 import { getChat } from '@/services/historyService';
 
 const ChatHandler = async ({ params }: { params: { chatid: string } }) => {
@@ -12,8 +13,8 @@ const ChatHandler = async ({ params }: { params: { chatid: string } }) => {
 
   const chat = await getChat(chatid);
 
-  if (chat) {
-    redirect(`/${chat.data?.group}/chat/${chatid}`);
+  if (isWonkSuccess(chat)) {
+    redirect(`/${chat.data.group}/chat/${chatid}`);
   }
 
   // If the chat is not found, redirect to the home page
