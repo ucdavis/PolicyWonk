eea08fdd8d3680e5d39f1c3cc1f2ae53d49b9a1f
Cal <cydoval@mac.com>
2025-06-12 09:42:34 -0700

    changing height of logo

--- web/src/components/layout/siteBrand.tsx
+++ web/src/components/layout/siteBrand.tsx
@@ -25,7 +25,7 @@ const SiteBrand = () => {
       </div>
       <div className='text-end'>
         <Link href={link} target='_blank' rel='noopener noreferrer'>
-          <Image src={logoPath} alt='Logo' width={100} height={21} />
+          <Image src={logoPath} alt='Logo' width={100} height={100} />
         </Link>
       </div>
     </div>
