492e9ee5eaa22ad8a1145e9d091f86256af21e50
aristachauhan <aristachauhan@gmail.com>
2026-02-04 22:44:50 +0000

    url updates now

--- web/src/components/chat/main.tsx
+++ web/src/components/chat/main.tsx
@@ -25,12 +25,12 @@ const MainContent = () => {
   React.useEffect(() => {
     if (
       // on first response from AI
-      pathname === '/chat/new' &&
+      pathname === `/${aiState.group}/chat/new` &&
       aiState.id !== '' // id is set only once the chat has been saved to the db
     ) {
       // reloads the sidebar, which repulls from the db now that the chat has been saved
       router.refresh();
-      window.history.pushState({}, '', `/chat/${aiState.id}`);
+      window.history.pushState({}, '', `/ucdavis/chat/${aiState.id}`); // change to group
     }
   }, [aiState.messages, router, aiState.id, pathname]);
 
--- web/src/components/layout/collapsibleSidebar.tsx
+++ web/src/components/layout/collapsibleSidebar.tsx
@@ -67,9 +67,7 @@ const CollapsibleSidebarComponent: React.FC<CollapsibleSidebarProps> = ({
               <Link
                 className='btn btn-icon btn-lg btn-link'
                 onClick={() => {
-                  if (window.location.pathname === newChatUrl) {
-                    window.location.assign(newChatUrl); // next navigation prevents reloading the same page
-                  } else if (!isLargeScreen) {
+                  if (!isLargeScreen) {
                     closeSidebar();
                   }
                 }}
